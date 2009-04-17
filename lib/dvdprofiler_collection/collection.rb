508bb7d89baf4a05ab54d04cb06b85a827908ed3
Roy Wright <roy@wright.org>
2009-04-16 19:42:54 -0500

    added collection_spec and fixed some problems in Collection

--- lib/dvdprofiler_collection/collection.rb
+++ lib/dvdprofiler_collection/collection.rb
@@ -3,10 +3,11 @@
 class Collection
   # various regexes used to clean up a title for matching purposes.
   # used in TITLE_REPLACEMENTS hash below
-  PUNCTUATION = /[\?\:\!\"\'\,\.\-\/\*]/
+  PUNCTUATION = /[\?\:\!\"\'\,\.\-\/\*\~\;\@\#\$\%\^]/
   HTML_ESCAPES = /\&[a-zA-Z]+\;/
   SQUARE_BRACKET_ENCLOSURES = /\[.*?\]/
   PARENTHESIS_ENCLOSURES = /\(.*?\)/
+  CURLY_BRACKET_ENCLOSURES = /\{.*?\}/
   MULTIPLE_WHITESPACES= /\s+/
   STANDALONE_AMPERSAND = /\s\&\s/
   WIDESCREEN = /widescreen/i
@@ -21,6 +22,7 @@ class Collection
   TITLE_REPLACEMENTS = [
     { SQUARE_BRACKET_ENCLOSURES => ''      },
     { PARENTHESIS_ENCLOSURES    => ''      },
+    { CURLY_BRACKET_ENCLOSURES  => ''      },
     { STANDALONE_AMPERSAND      => ' and ' },
     { HTML_ESCAPES              => ' '     },
     { WIDESCREEN                => ' '     },
@@ -29,19 +31,69 @@ class Collection
     { MULTIPLE_WHITESPACES      => ' '     },
   ]
 
-  attr_reader :isbn_dvd_hash, :title_isbn_hash, :isbn_title_hash
+  # == Synopsis
+  # The titles found between LMCE's Amazon lookup and DVDProfiler sometimes differ in
+  # whether or not a prefix of "The", "A", or "An" is included in the title.  Here we
+  # create an Array of possible titles with and without these prefix words.
+  def Collection.title_permutations(base_title)
+    titles = []
+    prefixes = ['the', 'a', 'an']
+    if base_title =~ /^(\S+)\s+(\S.*)$/
+      base_title = $2 if prefixes.include?($1)
+    end
+    unless base_title.nil? || base_title.empty?
+      titles << base_title
+      prefixes.each do |prefix|
+        titles << "#{prefix} #{base_title}"
+      end
+    end
+    titles.uniq
+  end
 
-  @filespec = nil
+  # == Synopsis
+  # the titles found between LMCE's Amazon lookup and DVDProfiler quite often differ in the
+  # inclusion of punctuation and capitalization.  So we create a pattern of lower case words
+  # without punctuation and with single spaces between words.
+  def Collection.title_pattern(src_title)
+    title = nil
+    unless src_title.nil?
+      title = src_title.dup
+      title.downcase!
+      TITLE_REPLACEMENTS.each do |replacement|
+        replacement.each do |regex, value|
+          title.gsub!(regex, value)
+        end
+      end
+      title.strip!
+    end
+    title
+  end
 
-  def initialize(filename, logger)
+  # hash with isbn as key and dvd hash as value
+  # isbn_dvd_hash[isbn] => dvd_hash
+  attr_reader :isbn_dvd_hash
+
+  # hash with title as key and array of isbn strings as value
+  # title_isbn_hash[title] => [isbn,...]
+  attr_reader :title_isbn_hash
+
+  # hash with isbn as key and title as value
+  # isbn_title_hash[isbn] => title
+  attr_reader :isbn_title_hash
+
+  # filename => filespec to Collection.xml
+  # logger => nil or logger instance
+  def initialize(filename, logger=nil)
     @title_isbn_hash = Hash.new
     @isbn_dvd_hash = Hash.new
     @isbn_title_hash = Hash.new
     @filespec = filename
     @logger = OptionalLogger.new(logger)
-    reload
+    load
     save
   end
+
+  protected
 
   # save as a collection.yaml file unless the existing
   # collection.yaml is newer than the collection.xml
@@ -68,64 +120,79 @@ class Collection
 
   # load the collection from the collection.yaml if it exists,
   # otherwise from the collection.xml
-  def reload
-    @title_isbn_hash.clear
-    @isbn_dvd_hash.clear
-    @isbn_title_hash.clear
-    collection = Hash.new
+  def load
     yaml_filespec = @filespec.ext('.yaml')
     if File.exist?(yaml_filespec) && (File.mtime(yaml_filespec) > File.mtime(@filespec))
-      @logger.info { "Loading #{yaml_filespec}" }
-      data = YAML.load_file(yaml_filespec)
-      @title_isbn_hash = data[:title_isbn_hash]
-      @isbn_dvd_hash = data[:isbn_dvd_hash]
-      @isbn_title_hash = data[:isbn_title_hash]
+      load_from_yaml(yaml_filespec)
     else
-      elapsed_time = timer do
-        @logger.info { "Loading #{@filespec}" }
-        collection = XmlSimple.xml_in(@filespec, { 'KeyToSymbol' => true})
-      end
-      @logger.info { "XmlSimple.xml_in elapse time: #{elapsed_time.elapsed_time_s}" }
-      collection[:dvd].each do |dvd|
-        isbn = dvd[:id][0]
-        original_title = dvd[:title][0]
-        title = Collection.title_pattern(dvd[:title][0])
-        unless isbn.blank? || title.blank?
-          @title_isbn_hash[title] ||= []
-          @title_isbn_hash[title] << isbn
-          @isbn_title_hash[isbn] = original_title
-          dvd_hash = Hash.new
-          dvd_hash[:isbn] = isbn
-          dvd_hash[:title] = original_title
-          unless dvd[:actors].blank?
-            dvd_hash[:actors] = dvd[:actors].compact.collect {|a| a[:actor]}.flatten.compact.collect do |a|
-              name = []
-              name << a['FirstName'] unless a['FirstName'].blank?
-              name << a['MiddleName'] unless a['MiddleName'].blank?
-              name << a['LastName'] unless a['LastName'].blank?
-              info = Hash.new
-              info['name'] = name.join(' ')
-              info['role'] = a['Role']
-              info
-            end
-          end
-          dvd_hash[:genres] = dvd[:genres].collect{|a| a[:genre]}.flatten unless dvd[:genres].blank?
-          dvd_hash[:studios] = dvd[:studios].collect{|a| a[:studio]}.flatten unless dvd[:studios].blank?
-          dvd_hash[:productionyear] = [dvd[:productionyear].join(',')] unless dvd[:productionyear].blank?
-          dvd_hash[:rating] = [dvd[:rating].join(',')] unless dvd[:rating].blank?
-          dvd_hash[:runningtime] = [dvd[:runningtime].join(',')] unless dvd[:runningtime].blank?
-          dvd_hash[:released] = [dvd[:released].join(',')] unless dvd[:released].blank?
-          dvd_hash[:overview] = [dvd[:overview].join(',')] unless dvd[:overview].blank?
-          dvd_hash[:lastedited] = dvd[:lastedited][0] unless dvd[:lastedited].blank?
-          directors = find_directors(dvd[:credits])
-          dvd_hash[:directors] = directors unless directors.blank?
-          dvd_hash[:boxset] = dvd[:boxset] unless dvd[:boxset].blank?
-          dvd_hash[:mediatypes] = dvd[:mediatypes] unless dvd[:mediatypes].blank?
-          dvd_hash[:format] = dvd[:format] unless dvd[:format].blank?
-          @isbn_dvd_hash[isbn] = dvd_hash
-        end
+      load_from_xml(@filespec)
+    end
+  end
+
+  def load_from_xml(xml_filespec)
+    collection = Hash.new
+    elapsed_time = timer do
+      @logger.info { "Loading #{xml_filespec}" }
+      collection = XmlSimple.xml_in(xml_filespec, { 'KeyToSymbol' => true})
+    end
+    @logger.info { "XmlSimple.xml_in elapse time: #{elapsed_time.elapsed_time_s}" }
+    collection[:dvd].each do |dvd|
+      isbn = dvd[:id][0]
+      original_title = dvd[:title][0]
+      title = Collection.title_pattern(dvd[:title][0])
+      unless isbn.blank? || title.blank?
+        @title_isbn_hash[title] ||= []
+        @title_isbn_hash[title] << isbn
+        @isbn_title_hash[isbn] = original_title
+        @isbn_dvd_hash[isbn] = select_dvd_info(dvd, isbn, original_title)
       end
     end
+  end
+
+  def load_from_yaml(yaml_filespec)
+    @logger.info { "Loading #{yaml_filespec}" }
+    data = YAML.load_file(yaml_filespec)
+    @title_isbn_hash = data[:title_isbn_hash]
+    @isbn_dvd_hash   = data[:isbn_dvd_hash]
+    @isbn_title_hash = data[:isbn_title_hash]
+  end
+
+  def select_dvd_info(dvd, isbn, original_title)
+    directors = find_directors(dvd[:credits])
+    dvd_hash = Hash.new
+    dvd_hash[:isbn]           = isbn
+    dvd_hash[:title]          = original_title
+    dvd_hash[:actors]         = find_actors(dvd[:actors])
+    dvd_hash[:genres]         = dvd[:genres].collect{|a| a[:genre]}.flatten unless dvd[:genres].blank?
+    dvd_hash[:studios]        = dvd[:studios].collect{|a| a[:studio]}.flatten unless dvd[:studios].blank?
+    dvd_hash[:productionyear] = [dvd[:productionyear].join(',')] unless dvd[:productionyear].blank?
+    dvd_hash[:rating]         = [dvd[:rating].join(',')] unless dvd[:rating].blank?
+    dvd_hash[:runningtime]    = [dvd[:runningtime].join(',')] unless dvd[:runningtime].blank?
+    dvd_hash[:released]       = [dvd[:released].join(',')] unless dvd[:released].blank?
+    dvd_hash[:overview]       = [dvd[:overview].join(',')] unless dvd[:overview].blank?
+    dvd_hash[:lastedited]     = dvd[:lastedited][0] unless dvd[:lastedited].blank?
+    dvd_hash[:directors]      = directors unless directors.blank?
+    dvd_hash[:boxset]         = dvd[:boxset] unless dvd[:boxset].blank?
+    dvd_hash[:mediatypes]     = dvd[:mediatypes] unless dvd[:mediatypes].blank?
+    dvd_hash[:format]         = dvd[:format] unless dvd[:format].blank?
+    dvd_hash
+  end
+
+  def find_actors(dvd_actors)
+    actors = nil
+    unless dvd_actors.blank?
+      actors = dvd_actors.compact.collect {|a| a[:actor]}.flatten.compact.collect do |a|
+        name = []
+        name << a['FirstName'] unless a['FirstName'].blank?
+        name << a['MiddleName'] unless a['MiddleName'].blank?
+        name << a['LastName'] unless a['LastName'].blank?
+        info = Hash.new
+        info['name'] = name.join(' ')
+        info['role'] = a['Role']
+        info
+      end
+    end
+    actors
   end
 
   def find_directors(dvd_credits)
@@ -146,41 +213,6 @@ class Collection
     rescue
     end
     directors
-  end
-
-  # == Synopsis
-  # The titles found between LMCE's Amazon lookup and DVDProfiler sometimes differ in
-  # whether or not a prefix of "The", "A", or "An" is included in the title.  Here we
-  # create an Array of possible titles with and without these prefix words.
-  def Collection.title_permutations(base_title)
-    titles = []
-    unless base_title.nil? || base_title.empty?
-      titles << base_title
-      ['the', 'a', 'an'].each do |prefix|
-        titles << "#{prefix} " + base_title unless base_title =~ /^#{prefix}\s/
-        titles << $1 if base_title =~ /^#{prefix}\s(.*)$/
-      end
-    end
-    titles
-  end
-
-  # == Synopsis
-  # the titles found between LMCE's Amazon lookup and DVDProfiler quite often differ in the
-  # inclusion of punctuation and capitalization.  So we create a pattern of lower case words
-  # without punctuation and with single spaces between words.
-  def Collection.title_pattern(src_title)
-    title = nil
-    unless src_title.nil?
-      title = src_title.dup
-      title.downcase!
-      TITLE_REPLACEMENTS.each do |replacement|
-        replacement.each do |regex, value|
-          title.gsub!(regex, value)
-        end
-      end
-      title.strip!
-    end
-    title
   end
 
 end
